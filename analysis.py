8cc7df94f34b66301a646853abb9032fe8e53108
Yinan-Zhang <yinan.zhang.gr@dartmouth.edu>
2015-08-04 15:32:15 -0400

    framework

---
+++ analysis.py
@@ -0,0 +1,37 @@
+
+class Structure:
+	'''A hanging mobile structure which is a set of points and their masses'''
+	def __init__(self, nodes, mass):
+		self.nodes = nodes;
+		self.mass = mass;     # The mass of the i-th node is self.mass[i]
+
+	def internal_forces(self):
+		'''get internal forces.
+		A force is defined as a tuple: (a,b) meaning the force from a on b.
+		@return a list of tuples;
+		'''
+		pass;
+
+	def external_forces(self):
+		'''get external forces'''
+		pass;
+
+	def inequality_constraints_ub(self):
+		'''@return A_ub, b_ub'''
+		pass;
+
+	def inequality_constraints_lb(self):
+		'''@return A_lb, b_lb'''
+		pass;
+
+	def equality_constraints(self):
+		'''@return A_eq, b_eq'''
+		pass;
+
+	def unknown(self):
+		'''build X in this method.'''
+		pass;
+
+	def objective_function(self):
+		'''@return C'''
+		pass;
---
+++ lpsolver.py
@@ -0,0 +1,17 @@
+
+
+
+
+def intlinprog(C, A_ub, b_ub, A_lb, b_lb, A_eq, b_eq, i):
+	'''
+	Solve the interget linear programming problem:
+	min C^T * x
+
+	subject to:
+			A_ub * x <= b_ub
+			A_lb * x >= b_lb
+			A_eq * x == b_eq
+			x[0:i] \in {0,1}
+	'''
+	pass;
+
